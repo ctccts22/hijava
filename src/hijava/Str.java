22035fb0dd5a896304eb086ee22ae3e0b3617d35
ctcct <ctcct@Gyuno>
2022-10-16 23:47:33 +0900

    Second commit

--- src/hijava/Str.java
+++ src/hijava/Str.java
@@ -3,8 +3,18 @@ package hijava;
 public class Str {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		byte b = 1;
+//		char c = 'A';
+		int i = 10000000;
+		long l = 1234567;
+		
+		System.out.println(i + l + 10);
+		System.out.println(i + l + b + 10);
+		
+		char j = (char)i;
+		System.out.println(j);
+		
+		System.out.println(Math.pow(2, 3));
 	}
 
 }
